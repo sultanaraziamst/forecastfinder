01abbc8b3d754f1851e8a0f626a948ab83d13ed2
sultanaraziamst <sultanaraziaofficial@gmail.com>
2024-07-07 17:07:27 +0600

    addparams

--- app.py
+++ app.py
@@ -14,3 +14,20 @@ API_URL = "http://api.openweathermap.org/data/2.5/weather"
 app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route ('/weather', methods=['POST'])
+
+def get_weather():
+    city = request.form['city']
+    params = {
+        'q':city,
+        'appid': API_KEY,
+        'units': 'metric'
+    }
+
+    response = request.get(API_URL, params=params)
+    weather_data = response.json()
+
+    if weather_data.get('cod') != 200:
+        return render_template('index.html', error=weather_data.get('message'))
